69cb10a55c12c28d2dd3f028543dda7685942af3
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2020-10-02 17:11:28 -0400

    Working on hero controller

--- m4herosightings/src/main/java/com/sg/m4herosightings/M4herosightingsApplication.java
+++ m4herosightings/src/main/java/com/sg/m4herosightings/M4herosightingsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class M4herosightingsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(M4herosightingsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(M4herosightingsApplication.class, args);
+    }
 
 }
